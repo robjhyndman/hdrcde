08ae961dedcd246bb337c1f699df0c86d3df1cba
Rob J Hyndman <robjhyndman@gmail.com>
2018-12-17 10:51:13 +1100

    Formatting

--- R/hdrscatterplot.R
+++ R/hdrscatterplot.R
@@ -24,65 +24,67 @@
 #' @keywords smooth distribution hplot
 #' @examples
 #'
-#' x <- c(rnorm(200,0,1),rnorm(200,4,1))
-#' y <- c(rnorm(200,0,1),rnorm(200,4,1))
-#' hdrscatterplot(x,y)
-#'
+#' x <- c(rnorm(200, 0, 1), rnorm(200, 4, 1))
+#' y <- c(rnorm(200, 0, 1), rnorm(200, 4, 1))
+#' hdrscatterplot(x, y)
 #' @export hdrscatterplot
-hdrscatterplot <- function(x, y, levels=c(1,50,99),
-  kde.package=c("ash","ks"), noutliers=NULL)
-{
+hdrscatterplot <- function(x, y, levels = c(1, 50, 99), kde.package = c("ash", "ks"), noutliers = NULL) {
   levels <- sort(levels)
-  if(missing(y))
+  if (missing(y)) {
     data <- x
-  else
-  {
-    data <- data.frame(x=x,y=y)
+  } else {
+    data <- data.frame(x = x, y = y)
     names(data) <- make.names(c(deparse(substitute(x)), deparse(substitute(y))))
   }
 
   vnames <- names(data)
 
-  den <- hdr.2d(data[,1], data[,2], prob=levels, kde.package=kde.package)
+  den <- hdr.2d(data[, 1], data[, 2], prob = levels, kde.package = kde.package)
 
   region <- numeric(NROW(data)) + 100
-  for(i in seq_along(levels))
-    region[den$fxy > den$falpha[i]] <- 100-levels[i]
-  if(is.null(noutliers))
+  for (i in seq_along(levels))
+    region[den$fxy > den$falpha[i]] <- 100 - levels[i]
+  if (is.null(noutliers)) {
     noutliers <- sum(region > max(levels))
+  }
 
   noutliers <- min(noutliers, NROW(data))
 
 
-  xlim <- diff(range(data[,1]))
-  ylim <- diff(range(data[,2]))
+  xlim <- diff(range(data[, 1]))
+  ylim <- diff(range(data[, 2]))
 
   # Construct region factor
-  levels <- sort(unique(region[region < 100]), decreasing=TRUE)
+  levels <- sort(unique(region[region < 100]), decreasing = TRUE)
   levels <- c(levels, 100)
-  data$Region <- factor(region, levels=levels,
-      labels=c(paste(head(levels,-1)), ">99"))
+  data$Region <- factor(region,
+    levels = levels,
+    labels = c(paste(head(levels, -1)), ">99")
+  )
   # Sort data so the larger regions go first (other than outliers)
   k <- region
-  k[region==100] <- 0
-  ord <- order(k, decreasing=TRUE)
-  data <- data[ord,]
+  k[region == 100] <- 0
+  ord <- order(k, decreasing = TRUE)
+  data <- data[ord, ]
 
-  if(noutliers > 0)
+  if (noutliers > 0) {
     outliers <- order(den$fxy[ord])[seq(noutliers)]
+  }
 
-  p <- ggplot2::ggplot(data, ggplot2::aes_string(vnames[1],vnames[2])) +
-      ggplot2::geom_point(ggplot2::aes(col=Region))
+  p <- ggplot2::ggplot(data, ggplot2::aes_string(vnames[1], vnames[2])) +
+    ggplot2::geom_point(ggplot2::aes(col = Region))
   p <- p + ggplot2::scale_colour_manual(
-      name="HDRs",
-      breaks=c(paste(head(sort(levels),-1)), ">99"),
-      values=c(RColorBrewer::brewer.pal(length(levels),"YlOrRd")[-1],"#000000"))
+    name = "HDRs",
+    breaks = c(paste(head(sort(levels), -1)), ">99"),
+    values = c(RColorBrewer::brewer.pal(length(levels), "YlOrRd")[-1], "#000000")
+  )
 
-  #Show outliers
-  if(noutliers > 0)
-  {
-    p <- p + ggplot2::annotate("text", x = data[outliers,1]+xlim/50, y=data[outliers,2]+ylim/50,
-                      label=rownames(data)[outliers],col='blue',cex=2.5)
+  # Show outliers
+  if (noutliers > 0) {
+    p <- p + ggplot2::annotate("text",
+      x = data[outliers, 1] + xlim / 50, y = data[outliers, 2] + ylim / 50,
+      label = rownames(data)[outliers], col = "blue", cex = 2.5
+    )
   }
   return(p)
 }
